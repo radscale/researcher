57d381396625d5378810c15c225a085ba4018739
Krykowski <marcin.krykowski@gmail.com>
2018-10-27 13:31:00 +0200

    Spring Boot basic application

---
+++ backend/university/src/main/java/com/researcher/university/ResearcherApplication.java
@@ -0,0 +1,12 @@
+package com.researcher.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearcherApplication.class, args);
+	}
+}
