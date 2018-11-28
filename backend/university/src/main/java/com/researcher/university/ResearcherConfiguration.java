f3bb35e1d662f4588ce0cc4fe44101d3987101f5
Adrian Hernik <radscale@radscale.co>
2018-11-28 03:24:52 +0100

    Add CORS-enabled configuration for the local environment

---
+++ backend/university/src/main/java/com/researcher/university/ResearcherConfiguration.java
@@ -0,0 +1,22 @@
+package com.researcher.university;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class ResearcherConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("http://localhost:8081");
+            }
+        };
+    }
+}
